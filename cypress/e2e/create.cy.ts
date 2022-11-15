a5e107e9f8634989b9e0f726869eaa9231325ff1
dagbolade <37887398+dagbolade@users.noreply.github.com>
2022-11-15 13:32:35 +0000

    week 6 lesson completed

--- cypress/e2e/create.cy.ts
+++ cypress/e2e/create.cy.ts
@@ -1,6 +1,6 @@
 describe("Create Room Test", () => {
   beforeEach(() => {
-    cy.visit("http://localhost:3000/create");
+    cy.visit("/create");
   });
 
   it("prevents a in-valid from from being submitted", () => {
@@ -17,6 +17,20 @@ describe("Create Room Test", () => {
     cy.getByData("capacity-error").should("exist");
     cy.getByData("type-error").should("exist");
   });
+
+  it("creates a new room", () => {
+    cy.get("[value='RM']").click();
+    cy.get("[value='pc']").click();
+    cy.getByData("number-input").type("1");
+    cy.getByData("capacity-input").type("50");
+    cy.getByData("submit-button").click();
+    cy.getByData("success-alert").should("exist");
+    cy.visit("/");
+    cy.getByData("room-item").should("have.length", 19);
+    cy.getByData("room-item").contains(/RM1/);
+  });
+
+
 
   it("removes errors when correct values are entered", () => {
     cy.getByData("submit-button").click();
